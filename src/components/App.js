bd556d37be0b89ddfe62e5b0154fb102a29fbad3
RocioGL33 <rochisolchis.08@gmail.com>
2023-01-10 16:00:31 -0300

    blank spaces fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
-import React, {useEffect} from 'react';
-import Header from './Header';
-import Content from './Content';
-import AOS from 'aos';
-
-
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Content from "./Content";
+import AOS from "aos";
 
 function App() {
-  useEffect(()=>{
-   AOS.init();
-   AOS.refresh(); 
-  },[])
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <>
       <Header />
-      <Content/>
+      <Content />
     </>
   );
 }

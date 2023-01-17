b5dd1376d8da121d58f87df5b38bd0ad1c9d495f
solanamr <romerosolana@gmail.com>
2023-01-17 20:20:52 -0300

    maintanence page fixed

--- src/components/maintenance/Maintenance.js
+++ src/components/maintenance/Maintenance.js
@@ -1,39 +1,32 @@
 import React from "react";
-import { ProgressBar } from "react-loader-spinner";
 import logoBetriax from "../../assets/img/logo.png";
 import P2P from "../../assets/img/Plataforma1.png";
 import { Link } from "react-router-dom";
+import BarLoader from "react-spinners/BarLoader";
 
-export default function Maintenance() {
+const Maintenance = () => {
   return (
     <div className="maintenance">
       <div className="card">
         <Link className="link-to-main" to={"/"}>
           <img alt="logo-betriax" className="logo-betriax" src={logoBetriax} />
         </Link>
-        <h3>Página en mantenimiento...</h3>
-        <ProgressBar
-          height="80"
-          width="80"
-          ariaLabel="progress-bar-loading"
-          wrapperStyle={{
-            position: "center",
-            left: "0px",
-            top: "0px",
-            "z-index": "1",
-            "text-align": "center",
-            "justify-content": "center",
-            "margin-left": "auto",
-            "margin-right": "auto",
-            height: "25%",
+        <h3>Página en mantenimiento</h3>
+        
+        <BarLoader
+          color="#1243e3"
+          cssOverride={{
+            marginRight: "auto",
+            marginLeft: "auto",
           }}
-          wrapperClass="progress-bar-wrapper"
-          borderColor="#1238B5"
-          barColor="#51E5FF"
+          height={4}
+          speedMultiplier={0}
         />
       </div>
       <div className="blur"></div>
       <img alt="maintence-view" className="img-p2p" src={P2P} />
     </div>
   );
-}
+};
+
+export default Maintenance;

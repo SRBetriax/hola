5247a38f1508d1987521253daab800224ef96f48
Enzo Sanchez <96807793+Enzos97@users.noreply.github.com>
2023-01-19 00:35:28 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,1 @@
-//export const currencyUrl = `https://api.currencylayer.com/live?access_key=${process.env.REACT_APP_CURRENCY_API_ACCESS_KEY}&currencies=EUR,COP,PEN,MXN,CLP,ARS,BOB&source=USD&format=1`;
-export const currencyUrl = `https://api.currencylayer.com/live?access_key=5d2e6f9b9ddb2e9e666c855856501ec6&currencies=EUR,COP,PEN,MXN,CLP,ARS,BOB&source=USD&format=1`;
+export const currencyUrl = `https://api.currencylayer.com/live?access_key=${process.env.REACT_APP_CURRENCY_API_ACCESS_KEY}&currencies=EUR,COP,PEN,MXN,CLP,ARS,BOB&source=USD&format=1`;

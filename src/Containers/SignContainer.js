0eb32ef4124bbfa9b6101cb042d652162f28777f
solanamr <romerosolana@gmail.com>
2023-01-30 17:27:02 -0300

    clean code

--- src/Containers/SignContainer.js
+++ src/Containers/SignContainer.js
@@ -1,22 +1,11 @@
 import { useParams } from "react-router";
-import Footer from "../components/footer/Footer";
-import PasswordRecovery from "../components/PasswordRecovery/PasswordRecovery";
-import Landing from "../pages/Landing";
 
 const SignContainer = () => {
 
     const params = useParams()
     return(
         <div>
-            {
-                params.data === "passwordRecovery" ? (
-                    <PasswordRecovery/>
-                ) : params.data === "token" ? (
-                    <Landing/>
-                ) : params.data === "holis" && (
-                    <Footer/>
-                )
-            }
+           
         </div>
     )
 }
